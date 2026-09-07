0a54f8182e1608774a75ddb1ee6765c5d7afce17
KyDung <kydung@example.com>
2026-09-07 23:10:15 +0700

    Remove auto-loading ad scripts

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,7 @@
-self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11383345
-}
-self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.registration.unregister());
+});
